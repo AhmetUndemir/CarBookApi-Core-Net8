69e7d1ee31ee91d726978b70852eb4fd9a0f4f23
Ahmet Ündemir <ahmetundemir@outlook.com>
2024-01-30 08:53:58 +0300

    Entityler tanımlandı.

--- Core/CarBook.Domain/Entities/Car.cs
+++ Core/CarBook.Domain/Entities/Car.cs
@@ -19,4 +19,7 @@ public class Car
     public byte Luggage { get; set; }
     public string Fuel { get; set; }
     public string BigImageUrl { get; set; }
+    public List<CarFeature> CarFeatures { get; set; }
+    public List<CarDescription> CarDescriptions { get; set; }
+    public List<CarPricing> CarPricings { get; set; }
 }
